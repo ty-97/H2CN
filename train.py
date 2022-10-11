2ab9c0e541f2457921bfb374f032a4ec2e119a46
ty-97 <59327553+ty-97@users.noreply.github.com>
2022-10-12 00:55:06 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import os
 
 cmd = "/home/tanyi/.conda/envs/vidnet/bin/python\
         main.py  somethingv1  RGB --arch resnet --net H2CN --num_segments 8 --gd 20 --lr 0.01 \
-        --lr_scheduler step --lr_steps 30 45 55 --epochs 60 --batch-size 8 \
+        --lr_steps 30 45 55 --epochs 60 --batch-size 8 \
         --wd 5e-4 --dropout 0.5 --consensus_type=avg --eval-freq=1 -j 16 \
         --npb"
 
